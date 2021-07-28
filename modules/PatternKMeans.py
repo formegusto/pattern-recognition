ad0033c1c25755b6363de70b5756b6c929fd2161
formegusto <formegusto@gmail.com>
2021-07-28 12:42:24 +0900

    백업

--- modules/PatternKMeans.py
+++ modules/PatternKMeans.py
@@ -1,3 +1,4 @@
+import operator
 from scipy.spatial import distance
 import random
 
@@ -37,6 +38,7 @@ class PatternKMeans:
         self.cluster_dict = {}
         self.cluster_info = pd.DataFrame()
         self.visual_datas = pd.DataFrame()
+        self.cluster_cost_map = {}
 
     def set_K(self, K):
         print("K is {}".format(K))
@@ -62,6 +64,7 @@ class PatternKMeans:
     # Remove Outlier
     def remove_outlier(self):
         datas = self.dimension_reduction()
+
         outlier_range = 1.5
 
         # sim_info scaler (Min-Max Normalization)
@@ -133,8 +136,18 @@ class PatternKMeans:
     def has_one_member(self):
         cluster_info = self.cluster_info.copy()
 
+        _count = cluster_info['label'].groupby(
+            cluster_info['label']).count()
+
+        Q1 = np.percentile(_count, 25)
+        Q3 = np.percentile(_count, 75)
+        IQR = Q3 - Q1
+        step = 1.5 * IQR
+
+        print(_count)
+        print("Q1: {}, Q3: {}, IQR: {}, step:{}".format(Q1, Q3, IQR, step))
         check = np.where(cluster_info['label'].groupby(
-            cluster_info['label']).count().values == 1)
+            cluster_info['label']).count().values <= (1))
         isIn = len(check[0]) >= 1
 
         if isIn:
@@ -148,40 +161,53 @@ class PatternKMeans:
     # init is "random, worst, best"
 
     def run(self, init_condition="worst", ECV=False, remove_outlier=False):
+        print("K Is {}".format(self.K))
+        divide_cluster = False
+
         if remove_outlier:
             self.remove_outlier()
 
         while True:
             sequence = 0
             row_datas = self.datas.T.copy()
-            self.cluster_dict = {}
+            print("클러스터 분리 여부 : {}".format(divide_cluster))
+            if not divide_cluster:
+                self.cluster_dict = {}
             prev_ecv = 0
             while True:
                 print("---Now {}---".format(sequence))
+                if not divide_cluster:
+                    self.cluster_cost_map = {}
 
                 # 첫 클러스터링
                 # Process , 첫 K 초기화
                 if sequence == 0:
                     cluster_index = []
-                    if init_condition == "random":
-                        init_k = random.randint(0, len(row_datas.index) - 1)
-                        init_cluster_index = row_datas.index[init_k]
-                    elif init_condition == "best":
-                        dr_datas = self.dimension_reduction()
-                        init_cluster_index = dr_datas.sort_values(
-                            ['x', 'y'], ascending=[True, False]).index[0]
-                    else:
-                        dr_datas = self.dimension_reduction()
-                        init_cluster_index = dr_datas.sort_values(
-                            ['x', 'y'], ascending=[False, True]).index[0]
 
-                    init_cluster = row_datas.loc[init_cluster_index].copy()
-                    cluster_index.append(init_cluster_index)
-                    self.cluster_dict[0] = init_cluster
+                    if not divide_cluster:
+                        print("First K Init")
+                        if init_condition == "random":
+                            init_k = random.randint(
+                                0, len(row_datas.index) - 1)
+                            init_cluster_index = row_datas.index[init_k]
+                        elif init_condition == "best":
+                            dr_datas = self.dimension_reduction()
+                            init_cluster_index = dr_datas.sort_values(
+                                ['x', 'y'], ascending=[True, False]).index[0]
+                        else:
+                            dr_datas = self.dimension_reduction()
+                            init_cluster_index = dr_datas.sort_values(
+                                ['x', 'y'], ascending=[False, True]).index[0]
+
+                        init_cluster = row_datas.loc[init_cluster_index].copy()
+                        cluster_index.append(init_cluster_index)
+                        self.cluster_dict[0] = init_cluster
+                        self.cluster_cost_map[0] = 0
+                    print("나머지")
                     # 나머지 K 초기화
                     sim_arr = []
                     sim_sort_arr = []
-                    for k in range(1, self.K):
+                    for k in range(len(self.cluster_dict.keys()), self.K):
                         sim_arr = ['dis_{}'.format(idx) for idx in range(0, k)]
                         sim_arr.extend(['cos_{}'.format(idx)
                                         for idx in range(0, k)])
@@ -211,6 +237,7 @@ class PatternKMeans:
                         ).index[0]
                         cluster_index.append(k_index)
                         self.cluster_dict[k] = row_datas.loc[k_index].copy()
+                        self.cluster_cost_map[k] = 0
 
                     # return sim_arr, sim_sort_arr, cluster_dict
 
@@ -223,11 +250,13 @@ class PatternKMeans:
                         if len(date_arr) != 0:
                             self.cluster_dict[k_num] = row_datas.loc[date_arr].mean(
                             ).values
+                        self.cluster_cost_map[k_num] = 0
 
                 self.cluster_info = pd.DataFrame()
                 self.visual_datas = pd.DataFrame()
                 self.labels = []
-
+                # print(self.cluster_dict)
+                # print(type(self.cluster_dict[0]))
                 # similar check
                 cols = ['distance', 'similarity']
                 rows = [idx for idx in range(0, self.K)]
@@ -278,11 +307,61 @@ class PatternKMeans:
             isInOne, findIdx = self.has_one_member()
             print("isInOne: {}, findIdx: {}".format(isInOne, findIdx))
 
-            if len(findIdx) == 0:
-                print("더 이상 제거할수가 없습니다.")
+            if self.get_ECV() >= 50:
                 break
+
+            if (len(findIdx) == 0) | (len(findIdx) >= (len(self.cluster_info['label'].unique()) - 2)):
+                print("더 이상 제거할수가 없습니다.")
+                print("-----COST MAP-----")
+                print(self.cluster_cost_map)
+
+                print("클러스터 분리를 시작합니다.")
+                cost_map = pd.DataFrame(index=self.cluster_cost_map.keys())
+                cost_map['count'] = self.cluster_info['label'].groupby(
+                    self.cluster_info['label']).count()
+                cost_map['cost'] = self.cluster_cost_map.values()
+
+                print("영향 요소 ===> {}".format(
+                    max(self.cluster_cost_map.items(), key=operator.itemgetter(1))[0]))
+
+                test_1 = cost_map.sort_values(
+                    by=["count", "cost"], ascending=False).index[0]
+                test_2 = cost_map.sort_values(
+                    by=["count"], ascending=[True]).index[0]
+
+                if cost_map.loc[test_2]['count'] == 1:
+                    test_2 = cost_map.sort_values(
+                        by=["count", "cost"], ascending=[True, False]).index[0]
+                print("Test Label", test_2)
+
+                idxes = self.cluster_info[
+                    self.cluster_info['label'] == test_1
+                ].index
+
+                new_dict_25 = pd.Series([np.percentile(
+                    self.datas.loc[idx], 25) for idx in self.datas[idxes].index], name="cluster-divide-25")
+                new_dict_75 = pd.Series([np.percentile(
+                    self.datas.loc[idx], 75) for idx in self.datas[idxes].index], name="cluster-divide-75")
+                self.cluster_cost_map[0] = 0
+                self.cluster_cost_map[1] = 0
+
+                self.remove_cluster_outlier([test_2])
+                self.cluster_dict = {}
+                self.cluster_dict[0] = new_dict_25
+                self.cluster_dict[1] = new_dict_75
+
+                divide_cluster = True
+                # break
             else:
+                divide_cluster = False
                 self.remove_cluster_outlier(findIdx)
+
+        cost_map = pd.DataFrame(index=self.cluster_cost_map.keys())
+        cost_map['count'] = self.cluster_info['label'].groupby(
+            self.cluster_info['label']).count()
+        cost_map['cost'] = self.cluster_cost_map.values()
+        return cost_map
+        # self.run(ECV=ECV, remove_outlier=remove_outlier)
         # if ECV == True:
         #     break
         # 두번째 부터는 중심정 찾기
@@ -435,6 +514,11 @@ class PatternKMeans:
                 pattern,
                 self.cluster_dict[k_num]
             ) ** 2
+            self.cluster_cost_map[k_num] += distance.euclidean(
+                pattern,
+                self.cluster_dict[k_num]
+            ) ** 2
+
         return self.WSS
 
     def get_ECV(self):
